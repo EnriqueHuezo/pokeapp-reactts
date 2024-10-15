afb7c1ca4e6d7519b93c0c3ee88b9680e06319bf
EnriqueHuezo <00237121@uca.edu.sv>
2024-10-15 12:39:03 -0600

    :art: chore: initialize React TS project with Tailwind and React Router DOM

---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { MainLayout } from './layout/MainLayout'
+
+const router = createBrowserRouter([
+  {
+    element: <MainLayout />,
+    children: [
+      {
+        path: '/',
+        element: <p>Home</p>
+      }
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
