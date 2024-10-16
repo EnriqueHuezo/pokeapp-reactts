cd1c2b59d61ef7ce32c67a68112ef9f8cfa27e27
EnriqueHuezo <00237121@uca.edu.sv>
2024-10-16 00:14:01 -0600

    :hammer: Update(Button and routes): update button design and add vercel routes

--- src/components/commons/Button.tsx
+++ src/components/commons/Button.tsx
@@ -1,12 +1,13 @@
 import { ButtonProps } from "../../types"
 
 export const Button: React.FC<ButtonProps> = ({ onClick, label, style, type = 'filled' }) => {
-  const typeButton = type === 'filled' ? 'bg-primary' : 'border border-primary bg-white text-red-700';
+  const typeButton = type === 'filled' ? 'bg-primary' : 'border border-primary bg-white';
   
   return (
     <button 
         onClick={onClick}
-        className={`${typeButton} px-4 py-2 text-white font-semibold rounded-md whitespace-nowrap ${style}`}>
+        className={`${typeButton} px-4 py-2 text-white font-semibold rounded-md whitespace-nowrap ${style}`}
+        style={{ color: type == 'outlined' ? "rgb(185 28 28)" : "white" }}>
         {label}
     </button>
   )
